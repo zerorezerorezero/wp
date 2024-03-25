d67eee69f2d80117f7de6e814e645fe3d7b3cedc
zerorezerorezero <zero1060130@gmail.com>
2024-03-25 22:16:11 +0800

    kaube

---
+++ javascript/HW3/3.isPrime.js
@@ -0,0 +1,11 @@
+function isPrime(n){
+    let d=1;
+    if(n==2)d=1;
+    else for(let i=2;i<n;i++){
+            if(n%i==0)d=0;
+            if(d==0)break;
+        }
+    if(d==0)console.log(n," is not a prime number.");
+    if(d==1)console.log(n," is a prime number.");
+}
+isPrime(15);
