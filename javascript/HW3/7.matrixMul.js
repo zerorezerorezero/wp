74898b01a03a3bf6be5cf87d948b2c3188c5d513
zerorezerorezero <zero1060130@gmail.com>
2024-04-13 16:55:40 +0800

    m

--- javascript/HW3/7.matrixMul.js
+++ javascript/HW3/7.matrixMul.js
@@ -2,14 +2,20 @@ var r=[];
 function matrixMul(a,b){
     if(a[0].length!=b.length){
         console.log("Invalid");
-        break;
     }
-
-    for(let i in a[0].length){
-        for(let j in b.length){
-            
+    else if(a[0].length==b.length){
+        r=new Array(a[0].length);
+        for(let i=0;i< r.length;i++){
+            r[i]=new Array(b.length).fill(0);
+        }
+        for(let i=0;i< a[0].length;i++){
+            for(let j=0;j< b.length;j++){
+                for(let k=i ;k< a[0].length;k++){
+                    r[i][j] +=a[i][k]*b[k][j];
+                }
+            }
         }
     }
 }
-matrixMul([[1,2],[3,4]],[[5,6],[7,8]]);
-console.log(s);
+matrixMul([[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]);
+console.log(r);
