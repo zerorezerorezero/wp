d25f8bbbfe594ea49cd7f1c535ccb90ff7587e24
zerorezerorezero <zero1060130@gmail.com>
2024-04-15 00:49:55 +0800

    m

--- javascript/HW3/5.vectorAdd.js
+++ javascript/HW3/5.vectorAdd.js
@@ -1,9 +1,11 @@
-var s=[0,0];
-
 function vectorAdd(a,b){
-    for(let i in a){
-        s[i]=a[i]+b[i];
+    var s=[];
+    if(a.length==b.length){
+        for(let i=0; i<a.length;i++){
+            s[i]=a[i]+b[i];
+        }
     }
+    return s;
 }
-vectorAdd([1,2],[3,4])
-console.log(s);
+var r=vectorAdd([1,2],[3,4]);
+console.log(r);
