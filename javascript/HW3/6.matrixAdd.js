b114b16e788723e232db62ff6819cc91929a280c
zerorezerorezero <zero1060130@gmail.com>
2024-04-15 00:44:19 +0800

    m

--- javascript/HW3/6.matrixAdd.js
+++ javascript/HW3/6.matrixAdd.js
@@ -1,11 +1,15 @@
-var s=[[0],[0]];
-
 function matrixAdd(a,b){
-    for(let i in a){
-        for(let j in a){
-            s[i][j]=(a[i][j]+b[i][j]);
+    var s=[];
+
+    if(a[0].length==b[0].length&&a.length==b.length){
+        for(let i=0; i<a[0].length;i++){
+            s[i]=[];
+            for(let j=0; j<a.length;j++){
+                s[i][j]=(a[i][j]+b[i][j]);
+            }
         }
     }
+    return s;
 }
-matrixAdd([[1,2],[3,4]],[[5,6],[7,8]]);
-console.log(s);
+var r=matrixAdd([[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]);
+console.log(r);
