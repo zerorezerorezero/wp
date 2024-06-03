1c874228865e1facc5a441e21ec150a1941213ea
zerorezerorezero <zero1060130@gmail.com>
2024-06-03 19:02:38 +0800

    k

--- 麥昆猜拳/js.js
+++ 麥昆猜拳/js.js
@@ -1,99 +1,120 @@
-var goal=10;
-        var Pscore=0;
-        var Cscore=0;
-        var Ps=document.getElementById("PS");
-        var Cs=document.getElementById("CS");
-        var PP=document.getElementById("PPic");
-        var CP=document.getElementById("CPic");
-        var BS=document.getElementById("Scissors");
-        var BR=document.getElementById("Rock");
-        var BP=document.getElementById("Paper");
+// Declaring variables
+let goal=10;
+let Pscore=0;
+let Cscore=0;
+let Ps=document.getElementById("PS");
+let Cs=document.getElementById("CS");
+let PP=document.getElementById("PPic");
+let CP=document.getElementById("CPic");
+let BS=document.getElementById("Scissors");
+let BR=document.getElementById("Rock");
+let BP=document.getElementById("Paper");
+let BReset=document.getElementById("BRst");
+let button;
+let title=document.getElementById("title");
+let CPU=0;
+let PLR=0;
+let evn;
 
-        function c(evn){
-            var button =evn.id;
-            var CPU=0;
-            var PLR=0;
+function c(evn){ // Main clicking event
+    button =evn.id;
+    PP.src="SL.png";
+    CP.src="WL.png";
+    title.innerText="..."
 
-            switch (button){
-                case "Scissors":
-                    PLR=0; break;
-                case "Rock":
-                    PLR=1; break;
-                case "Paper":
-                    PLR=2; break;
-            }
+    BS.disabled=true;
+    BR.disabled=true;
+    BP.disabled=true;
+    BReset.disabled=true;
 
-            CPU=Math.floor(Math.random()*3);
+    setTimeout(R,1000);
+}
 
-            PicSet(PLR,CPU);
+function PicSet(p,c){ // Setting pictures
+    switch (p){
+        case 0: 
+            PP.src="SS.png";break;
+        case 1:
+            PP.src="SR.png";break;
+        case 2:
+            PP.src="SP.png";break;
+    }
 
-            var title=document.getElementById("title");
-            if(CPU==PLR){
-                title.innerText="平手";
-            }
-            else if(PLR-CPU==1||PLR-CPU==-2){
-                title.innerText="勝";
-                Pscore++;
-            }
-            else {
-                title.innerText="敗";
-                Cscore++;
-            }
+    switch (c){
+        case 0: 
+            CP.src="WS.png";break;
+        case 1:
+            CP.src="WR.png";break;
+        case 2:
+            CP.src="WP.png";break;
+    }
+}
 
-            Ps.innerText=Pscore.toString();
-            Cs.innerText=Cscore.toString();
+function reset(){ // Reset everything
+    title.innerText="你是目白麥昆[漣漪窈窕淑女]，打敗目白麥昆[空之盡頭]即獲勝。(先取得10分)";
+    CPU=0;
+    PLR=0;
+    Pscore=0;
+    Cscore=0;
+    Ps.innerText=Pscore.toString();
+    Cs.innerText=Cscore.toString();
+    PP.src="Q.png";
+    CP.src="Q.png";
+    BS.disabled=false;
+    BR.disabled=false;
+    BP.disabled=false;
+}
 
-            if(Pscore==goal||Cscore==goal){
-                if(Pscore>Cscore){
-                    title.innerText="你贏了!"
-                    PP.src="SQwin.png";
-                    CP.src="WQlose.png";
+function R(){ // Show result
 
-                }
-                else {
-                    title.innerText="你輸了......"
-                    PP.src="SQlose.png";
-                    CP.src="WQwin.png";
-                }
+    switch (button){ // Distinguish witch button was clicked
+        case "Scissors":
+            PLR=0; break;
+        case "Rock":
+            PLR=1; break;
+        case "Paper":
+            PLR=2; break;
+    }
 
-                BS.disabled=true;
-                BR.disabled=true;
-                BP.disabled=true;
-            }
+    CPU=Math.floor(Math.random()*3); // Generate CPU's reaction
+
+    PicSet(PLR,CPU); // Send Player and CPU's answer to PicSet to set pictures 
+
+    if(CPU==PLR){ // Distinguish the situation
+        title.innerText="平手";
+    }
+    else if(PLR-CPU==1||PLR-CPU==-2){ // Ex: Player: Rock(1), CPU: Scissors(0). Or: Player: Scissors(0), CPU:Paper (2).
+        title.innerText="勝";
+        Pscore++;
+    }
+    else {
+        title.innerText="敗";
+        Cscore++;
+    }
+
+    Ps.innerText=Pscore.toString(); // Loading result of score to the page
+    Cs.innerText=Cscore.toString();
+
+    BS.disabled=false; // Buttons is ENABLE to be clicked again
+    BR.disabled=false;
+    BP.disabled=false;
+    BReset.disabled=false;
+
+    if(Pscore==goal||Cscore==goal){
+        if(Pscore>Cscore){
+            title.innerText="你贏了!"
+            PP.src="SQwin.png";
+            CP.src="WQlose.png";
+        }
+        else {
+            title.innerText="你輸了......"
+            PP.src="SQlose.png";
+            CP.src="WQwin.png";
         }
 
-        function PicSet(p,c){
-            switch (p){
-                case 0: 
-                    PP.src="SS.png";break;
-                case 1:
-                    PP.src="SR.png";break;
-                case 2:
-                    PP.src="SP.png";break;
-            }
-
-            switch (c){
-                case 0: 
-                    CP.src="WS.png";break;
-                case 1:
-                    CP.src="WR.png";break;
-                case 2:
-                    CP.src="WP.png";break;
-            }
-        }
-
-        function reset(){
-            title.innerText="你是目白麥昆[漣漪窈窕淑女]，打敗目白麥昆[空之盡頭]即獲勝。(先取得10分)";
-            CPU=0;
-            PLR=0;
-            Pscore=0;
-            Cscore=0;
-            Ps.innerText=Pscore.toString();
-            Cs.innerText=Cscore.toString();
-            PP.src="Q.png";
-            CP.src="Q.png";
-            BS.disabled=false;
-            BR.disabled=false;
-            BP.disabled=false;
-        }
+        BS.disabled=true;
+        BR.disabled=true;
+        BP.disabled=true;
+    }
+}
 
