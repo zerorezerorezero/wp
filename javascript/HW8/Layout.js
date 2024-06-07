bbc0d2a63814cbdc9a2290a5463136a467cac3fa
zerorezerorezero <zero1060130@gmail.com>
2024-06-07 14:17:49 +0800

    k

---
+++ javascript/HW8/Layout.js
@@ -0,0 +1,20 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var dropdown = document.getElementById('dropdown');
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('#toggleButton')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+};
